ae8672320e3c231503eb53e28eab8ac741c9b820
Adrian-ortiz0 <dxniel7328@gmail.com>
2024-11-12 18:20:52 -0500

    feat: :sparkles: I've added some details in order to practice react even more

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { MultipleCustomHook } from './MultipleCustomHook.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { MultipleCustomHook } from "./MultipleCustomHook.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <MultipleCustomHook />
-  </StrictMode>,
-)
+  </StrictMode>
+);
